cc51c9e67328c481c94d23a397456ca55f62243b
uagarwal <uagarwal@synamedia.com>
2021-04-24 09:27:24 +0530

    Made the code more modular

--- codebase/main.go
+++ codebase/main.go
@@ -12,6 +12,7 @@ import (
 
 	"wc-accounting-go/codebase/cfg"
 	"wc-accounting-go/codebase/model"
+	"wc-accounting-go/codebase/modules/login"
 )
 
 const (
@@ -19,49 +20,9 @@ const (
 	env            = "dev"
 )
 
-type (
-		LoginResp struct{
-			Message string
-			StatusCode int
-			FirstLogin string 
-		}
-)
-
 func main() {
 	fmt.Println("My Simple Server")
 	handleRequests()
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Login Attempt")
-	var bodyData []byte
-	var err error
-	var userCredMap map[string]string
-	if bodyData, err = ioutil.ReadAll(r.Body); err != nil {
-		w.Write([]byte("Error while reading the request"))
-		return
-	}
-	json.Unmarshal(bodyData, &userCredMap)
-	var user model.User
-	w.Header().Set("Content-Type", "application/json")
-	var loginResp LoginResp
-	if user, err = model.ReadUserDataWithUsernamePassword(userCredMap["Username"], userCredMap["Password"]); err != nil {
-		loginResp.StatusCode = http.StatusNotFound
-		loginResp.Message = "User not found"
-		loginResp.FirstLogin = ""
-		fmt.Println(err)
-	}
-	if (user != model.User{}) {
-		fmt.Println("User Found")
-		loginResp.StatusCode = http.StatusOK
-		loginResp.Message = fmt.Sprintf("Welcome %s",user.UserName)
-		if user.FirstLogin == 1{
-			loginResp.FirstLogin = "True"
-		} else {
-			loginResp.FirstLogin = "False"
-		}
-	}
-	json.NewEncoder(w).Encode(loginResp)
 }
 
 func handleRequests() {
@@ -79,6 +40,6 @@ func handleRequests() {
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
 	origins := handlers.AllowedOrigins([]string{"*"})
 
-	router.HandleFunc("/login", login)
+	router.HandleFunc("/login", login.Login)
 	log.Fatal(http.ListenAndServe(":9001", handlers.CORS(headers, methods, origins)(router)))
 }
---
+++ codebase/modules/login/login.go
@@ -0,0 +1,49 @@
+package login
+
+import(
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+
+	"wc-accounting-go/codebase/model"
+)
+
+type (
+	LoginResp struct{
+		Message string
+		StatusCode int
+		FirstLogin string 
+	}
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Login Attempt")
+	var bodyData []byte
+	var err error
+	var userCredMap map[string]string
+	if bodyData, err = ioutil.ReadAll(r.Body); err != nil {
+		w.Write([]byte("Error while reading the request"))
+		return
+	}
+	json.Unmarshal(bodyData, &userCredMap)
+	var user model.User
+	w.Header().Set("Content-Type", "application/json")
+	var loginResp LoginResp
+	if user, err = model.ReadUserDataWithUsernamePassword(userCredMap["Username"], userCredMap["Password"]); err != nil {
+		loginResp.StatusCode = http.StatusNotFound
+		loginResp.Message = "User not found"
+		loginResp.FirstLogin = ""
+		fmt.Println(err)
+	}
+	if (user != model.User{}) {
+		fmt.Println("User Found")
+		loginResp.StatusCode = http.StatusOK
+		loginResp.Message = fmt.Sprintf("Welcome %s",user.UserName)
+		if user.FirstLogin == 1{
+			loginResp.FirstLogin = "True"
+		} else {
+			loginResp.FirstLogin = "False"
+		}
+	}
+	json.NewEncoder(w).Encode(loginResp)
+}
